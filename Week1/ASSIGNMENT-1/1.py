80943cc42c324ee1d075f1e484057e388489d5b0
22bca233 <127925449+22bca233@users.noreply.github.com>
2023-06-29 11:13:22 +0530

    Add files via upload

---
+++ Week1/ASSIGNMENT-1/1.py
@@ -0,0 +1,14 @@
+#1.Write a Python script to enter an y number and check its prime or not
+def primeornot(n):
+    ans=True
+    for i in range(2,n):
+        if(n%2==0):
+            ans=False
+    if(ans==True):
+        print("Number is prime!!")
+    else:
+        print("Number is not prime!!")
+
+
+num=int(input("Enter a number:"))
+primeornot(num)
